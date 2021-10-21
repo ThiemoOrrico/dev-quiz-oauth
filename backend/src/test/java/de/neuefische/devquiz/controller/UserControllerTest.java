920115593a8fbe15ac9978b03514d1d5ef784fb1
Jan-Oke Mettendorf <90198828+Jan-OkeMettendorf@users.noreply.github.com>
2021-10-21 12:44:42 +0200

    Expand AppUserControllerTest invalid & expired token

--- backend/src/test/java/de/neuefische/devquiz/controller/UserControllerTest.java
+++ backend/src/test/java/de/neuefische/devquiz/controller/UserControllerTest.java
@@ -2,17 +2,29 @@ package de.neuefische.devquiz.controller;
 
 import de.neuefische.devquiz.security.model.AppUser;
 import de.neuefische.devquiz.security.repo.AppUserRepo;
+import de.neuefische.devquiz.security.service.JWTUtilService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = "neuefische.devquiz.jwt.secret=super-fancy-secret-from-jan-oke")
 class UserControllerTest {
 
     @Autowired
@@ -24,7 +36,7 @@ class UserControllerTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
+    private final JWTUtilService jwtUtilService = mock(JWTUtilService.class);
 
     @Test
     void testGetLoggedInUserDataWithValidToken() {
@@ -38,6 +50,40 @@ class UserControllerTest {
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
     }
 
+    @Test
+    void testGetLoggedInUserDataWithExpiredToken() {
+        //GIVEN
+
+        when(jwtUtilService.createToken(new HashMap<>(), "some-username")).thenReturn(
+                Jwts.builder()
+                        .setSubject("some-username")
+                        .setIssuedAt(Date.from(Instant.now().minus(Duration.ofHours(5))))
+                        .setExpiration(Date.from(Instant.now().minus(Duration.ofHours(4))))
+                        .signWith(SignatureAlgorithm.HS256, "neuefische.devquiz.jwt.secret=super-fancy-secret-from-jan-oke")
+                        .compact()
+        );
+
+        HttpHeaders token = getJWT();
+
+        //WHEN
+        ResponseEntity<String> response = restTemplate.exchange("/user/me", HttpMethod.GET, new HttpEntity<>(getJWT()), String.class);
+
+        //THEN
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    void testGetLoggedInUserDataWithInvalidToken() {
+        //GIVEN
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("ey.asden.sanfosih");
+
+        //WHEN
+        ResponseEntity<String> response = restTemplate.exchange("/user/me", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+        //THEN
+        assertThat(response.getStatusCode(), is(HttpStatus.FORBIDDEN));
+    }
 
 
     private HttpHeaders getJWT() {
